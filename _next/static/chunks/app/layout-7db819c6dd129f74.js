64e1c36b3528161ce22e500c5bf7dd7cb681cc49
jbukuts <jbukuts@gmail.com>
2024-04-05 18:54:10 +0000

    Deploying to gh-pages from @ jbukuts/nextjs-carbon-doc-template@87a5d7a2e84d5a96b86b8f37fd195cd593b71e28 🚀

---
+++ _next/static/chunks/app/layout-7db819c6dd129f74.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{3449:function(n,e,u){Promise.resolve().then(u.t.bind(u,9039,23))},9039:function(){}},function(n){n.O(0,[971,69,744],function(){return n(n.s=3449)}),_N_E=n.O()}]);
