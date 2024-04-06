4d031bd80a3a7c5d2299b5d2acc351dc33d13892
Jake Bukuts <jbukuts@Jakes-MBP.attlocal.net>
2024-04-06 14:35:05 -0400

    fix: remove unneded page

--- src/app/[locale]/[[...slug]]/page.tsx
+++ src/app/[locale]/[[...slug]]/page.tsx
@@ -1,16 +1,36 @@
 import pick from 'lodash/pick';
-import { Metadata, ResolvingMetadata } from 'next';
+import type { Metadata, ResolvingMetadata } from 'next';
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages, unstable_setRequestLocale } from 'next-intl/server';
 import { PageHeader } from '#/components/ui';
-import type { Locale } from '#/i18n';
 import compileContent from '#/lib/mdx/compile-content';
 import { SLUG_MAP } from '#/lib/velite';
 import { generateMergedMetadata } from '#/shared-metadata';
-import styles from './[...slug]/page.module.scss';
+import styles from './page.module.scss';
 
-interface LocalePageProps {
-  params: { locale: Locale };
+type Slug = { slug?: string[]; locale: string };
+
+interface PageProps {
+  params: Slug;
+}
+
+/**
+ * SSG function to determine slugs to prerender
+ *
+ * @see https://nextjs.org/docs/app/api-reference/functions/generate-static-params
+ */
+export function generateStaticParams(): Slug[] {
+  const slugs: string[] = Object.keys(SLUG_MAP);
+  return slugs.map((slug) => {
+    const splitSlug = slug.split('/');
+    const locale = splitSlug.shift();
+
+    // if (splitSlug.length === 0) splitSlug.push('');
+
+    console.log(locale, splitSlug);
+
+    return { slug: splitSlug, locale: locale! };
+  });
 }
 
 /**
@@ -19,52 +39,47 @@ interface LocalePageProps {
  * @see https://nextjs.org/docs/app/api-reference/functions/generate-metadata
  */
 export async function generateMetadata(
-  props: LocalePageProps,
+  props: PageProps,
   _parent: ResolvingMetadata
 ): Promise<Metadata> {
   const {
-    params: { locale }
+    params: { slug = [], locale }
   } = props;
 
-  const { title: t, toc_tree, updated, excerpt, desc } = SLUG_MAP[locale];
+  const fullSlug = [locale, ...slug].join('/');
+  const { title: t, toc_tree, updated, excerpt, desc } = SLUG_MAP[fullSlug];
 
   const title = t || toc_tree[0].title || undefined;
   const description = desc ? desc : excerpt;
-
-  const slug = `/${locale}`;
 
   return generateMergedMetadata({
     title,
     description,
     alternates: {
-      canonical: slug
+      canonical: fullSlug
     },
     twitter: { title, description },
     openGraph: {
       type: 'article',
       title,
       description,
-      url: slug,
+      url: fullSlug,
       locale,
       modifiedTime: updated
     }
   });
 }
 
-/**
- * Sourced from next-int static example
- *
- * @see https://next-intl-docs.vercel.app/docs/getting-started/app-router#static-rendering
- */
-export default async function LocalePage(props: LocalePageProps) {
+export default async function LabPage(props: PageProps) {
   const {
-    params: { locale }
+    params: { slug = [], locale }
   } = props;
 
   unstable_setRequestLocale(locale);
 
+  const fullSlug = [locale, ...slug].join('/');
   const { content, toc, frontmatter, breadcrumbs } = await compileContent(
-    locale,
+    fullSlug,
     { h1: () => null }
   );
 
