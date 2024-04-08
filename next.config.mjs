7876a0a646a2361e7fafb58b5b2b9835bb2c0b70
Jake Bukuts <jbukuts@gmail.com>
2024-04-08 15:08:22 -0400

    chore: bring back intl plugin

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
 // import fs from 'fs';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import createNextIntlPlugin from 'next-intl/plugin';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
+const withNextIntl = createNextIntlPlugin();
 
 // const SYMLINKS = ['content', 'fragments'];
 // const IS_DEV = process.env.NODE_ENV === 'development';
@@ -40,10 +42,7 @@ const nextConfig = {
     config.resolve.alias = {
       ...config.resolve.alias,
       sharp$: false,
-      'onnxruntime-node$': false,
-      // manually do this
-      // https://github.com/amannn/next-intl/blob/78f88d1493eb796498b7fb2c986536ac49cffda0/packages/next-intl/src/plugin.tsx#L95
-      'next-intl/config': path.resolve(config.context, './src/i18n.ts')
+      'onnxruntime-node$': false
     };
 
     return config;
@@ -78,4 +77,4 @@ const nextConfig = {
   }
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
