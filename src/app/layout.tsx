721499464c39163944889240cc4d00347724e03c
Jake Bukuts <jbukuts@gmail.com>
2024-04-08 23:48:08 -0400

    imp: SEO apple-icon, viewport items

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,11 @@
-import * as themes from '@carbon/themes';
-import type { Viewport, Metadata } from 'next';
+import type { Metadata } from 'next';
 import type { ReactNode } from 'react';
 import './globals.scss';
 import baseMetadata from '#/shared-metadata';
 
-// @ts-ignore
-const { g100, white } = themes;
-
 interface RootLayoutProps {
   children: ReactNode;
 }
-
-/**
- * Viewport object
- *
- * @see https://nextjs.org/docs/app/api-reference/functions/generate-viewport#the-viewport-object
- */
-export const viewport: Viewport = {
-  themeColor: [
-    { media: '(prefers-color-scheme: light)', color: white.background },
-    { media: '(prefers-color-scheme: dark)', color: g100.background }
-  ]
-};
 
 /**
  * SEO metadata
